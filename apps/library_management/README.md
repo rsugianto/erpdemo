136f1f03959a65b69fcef6f0d49b18c5a8d27dab
root <root@erpnextdemo.localdomain>
2017-04-07 09:12:31 +0700

    first commit

---
+++ apps/library_management/README.md
@@ -0,0 +1,7 @@
+## library_management
+
+App for managing Articles, Members, Memberships and Transactions for Libraries
+
+#### License
+
+GNU General Public License
---
+++ apps/library_management/library_management/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "library_management",
+			"color": "#589494",
+			"icon": "octicon action-book",
+			"type": "module",
+			"label": _("library_management")
+		}
+	]
